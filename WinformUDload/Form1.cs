b2771f65ade6fd76aebe92c5cb1b4e0e09fa496c
Chris <im.wp@foxmail.com>
2022-07-08 16:42:39 +0800

    私有文件

--- WinformUDload/Form1.cs
+++ WinformUDload/Form1.cs
@@ -16,14 +16,18 @@ using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using WinformUDload.Services;
 
 namespace WinformUDload
 {
     public partial class Form1 : FormBase
     {
+        private readonly FileLoadServices _fls = null;
+
         public Form1()
         {
             InitializeComponent();
+            _fls = new FileLoadServices();
         }
 
         private List<string> list = new List<string>();
@@ -37,33 +41,14 @@ namespace WinformUDload
             superGridControl1.PrimaryGrid.AllowRowHeaderResize = true;
             //superGridControl1.PrimaryGrid.Filter.Visible = true;
             superGridControl1.PrimaryGrid.SelectionGranularity = DevComponents.DotNetBar.SuperGrid.SelectionGranularity.Row;
-            superGridControl1.PrimaryGrid.DataSource = await GetWebFilesDataTable();
+            superGridControl1.PrimaryGrid.DataSource = await _fls.GetWebFilesDataTable();
             openFileDialog1.Multiselect = true;  // 允许多选
-        }
-
-        private async Task<DataTable> GetWebFilesDataTable()
-        {
-            List<FileInformation> fileInformations = await GetWebFiles();
-            return fileInformations.ToDataTable("fileInformations");
-
-        }
-
-        private async Task<List<FileInformation>> GetWebFiles()
-        {
-            using (HttpClient httpClient = new HttpClient())
-            {
-                var url = ConfigurationManager.AppSettings["GetInfo"].ToString();
-                var response = await httpClient.GetAsync(url);
-                string json = await response.Content.ReadAsStringAsync();
-                var fileInformations = JsonConvert.DeserializeObject<List<FileInformation>>(json);
-                return fileInformations;
-            }
         }
 
 
         private async void btnRefresh_ClickAsync(object sender, EventArgs e)
         {
-            superGridControl1.PrimaryGrid.DataSource = await GetWebFilesDataTable();
+            superGridControl1.PrimaryGrid.DataSource = await _fls.GetWebFilesDataTable();
         }
 
         private async void btnUpload_Click(object sender, EventArgs e)
@@ -76,7 +61,7 @@ namespace WinformUDload
                 for (int i = 0; i < fileNames.Length; i++)
                 {
                     string fileName = fileNames[i];
-                    FileUpLoadDto fileUpLoadDto = await FileUpload(fileName);
+                    FileUpLoadDto fileUpLoadDto = await _fls.FileUpload(fileName);
 
                     if (fileUpLoadDto.Status)
                     {
@@ -94,29 +79,6 @@ namespace WinformUDload
         }
 
         /// <summary>
-        ///   文件上传
-        /// </summary>
-        /// <param name="filePath">本地需要上传的文件路径</param>
-        /// <returns></returns>
-        private async Task<FileUpLoadDto> FileUpload(string filePath)
-        {
-            FileUpLoadDto fileUpLoadDto = null;
-            using (var httpClient = new HttpClient())
-            {
-                var fs = new FileStream(filePath, FileMode.Open, FileAccess.Read);
-                var fileName = Path.GetFileName(filePath);
-                var content = new MultipartFormDataContent();
-                //  Add 方法 第一个参数是content 
-                content.Add(new StreamContent(fs), "file", fileName);
-                var url = ConfigurationManager.AppSettings["Upload"].ToString();
-                var response = await httpClient.PostAsync(url, content);
-                var json = await response.Content.ReadAsStringAsync();
-                fileUpLoadDto = JsonConvert.DeserializeObject<FileUpLoadDto>(json);
-            }
-            return fileUpLoadDto;
-        }
-
-        /// <summary>
         ///  写日志到textbox当中
         /// </summary>
         /// <param name="msg"></param>
@@ -127,50 +89,12 @@ namespace WinformUDload
             RTxtMessage.AppendText("\r\n");
         }
 
-        /// <summary>
-        /// 下载文件
-        /// </summary>
-        /// <param name="fileName">文件名</param>
-        /// <returns></returns>
-        private async Task<ResultMessage> DownFile(string fileName)
-        {
-            ResultMessage resultMessage = null;
-            try
-            {
-                using (var client = new HttpClient())
-                {
-                    var url = ConfigurationManager.AppSettings["DownFile"].ToString();
-                    url += "?fileName=" + fileName;
-                    var response = await client.GetAsync(url);
-                    if (response.IsSuccessStatusCode)
-                    {
-                        using (var stream = await response.Content.ReadAsStreamAsync())
-                        {
-                            string winPath = Path.Combine(Path.GetDirectoryName(Assembly.GetEntryAssembly().Location), fileName);
-                            await stream.CopyToFileAsync(winPath);
-                            resultMessage = new ResultMessage { Status = true, Message = "成功" };
-                        }
-                    }
-                    else
-                    {
-                        resultMessage = new ResultMessage { Status = false, Message = response.ToString() };
-                    }
-                }
-            }
-            catch (Exception ex)
-            {
-                resultMessage = new ResultMessage { Status = false, Message = ex.Message };
-            }
-
-            return resultMessage;
-        }
-
         private async void btnDownload_Click(object sender, EventArgs e)
         {
             var fileName = GetDgvValue("FileName");
             if (fileName == null)
                 return;
-            ResultMessage resultMessage = await DownFile(fileName);
+            ResultMessage resultMessage = await _fls.DownFile(fileName);
             if (resultMessage.Status)
                 WriteMsg($"【{fileName}】 下载成功！ ");
             else
@@ -204,43 +128,6 @@ namespace WinformUDload
         }
 
         /// <summary>
-        /// 获取可以更新文件信息
-        /// </summary>
-        /// <returns></returns>
-        private async Task<List<FileInformation>> GetNeedDownFiles()
-        {
-            List<FileDownloadDto> list = new List<FileDownloadDto>();
-            string location = Assembly.GetEntryAssembly().Location;
-            List<FileInformation> fileInformations = await GetWebFiles();
-            string[] localPathArray = (from a in Directory.GetFiles(Path.GetDirectoryName(location))
-                                       join b in fileInformations on Path.GetFileName(a) equals b.FileName
-                                       select a).ToArray();
-
-            for (int i = 0; i < localPathArray.Length; i++)
-            {
-                string itemPath = localPathArray[i];
-                using (var fs = new FileStream(itemPath, FileMode.Open))
-                {
-                    list.Add(new FileDownloadDto
-                    {
-                        FileName = Path.GetFileName(itemPath),
-                        FileMd5 = fs.GetFileMD5()
-                    });
-                }
-            }
-
-            var needDownInfos = (from a in fileInformations
-                                 join b in list on a.FileName equals b.FileName
-                                 where a.FileName == b.FileName && a.FileMd5 != b.FileMd5
-                                 select a).
-                                 Concat(
-                                 from a in fileInformations
-                                 where !(from b in list select b).Any(b => a.FileName == b.FileName)
-                                 select a).ToList();
-            return needDownInfos;
-        }
-
-        /// <summary>
         /// 下载文件循环
         /// </summary>
         /// <param name="input"></param>
@@ -257,7 +144,7 @@ namespace WinformUDload
 
             input.ForEach(async f =>
             {
-                ResultMessage resultMessage = await DownFile(f.FileName);
+                ResultMessage resultMessage = await _fls.DownFile(f.FileName);
                 if (resultMessage.Status)
                     WriteMsg($"【{f.FileName}】 下载成功！ ");
                 else
@@ -269,7 +156,7 @@ namespace WinformUDload
 
         private async void btnDownlaodMore_Click(object sender, EventArgs e)
         {
-            var fileInformations = await GetNeedDownFiles();
+            var fileInformations = await _fls.GetNeedDownFiles();
             DownNeedFile(fileInformations);
         }
 
@@ -279,23 +166,18 @@ namespace WinformUDload
             if (fileName == null)
                 return;
 
-            if (MessageBox.Show($"确定要删除文明名【{fileName}】? 当前操作不可逆~", "", MessageBoxButtons.OKCancel) == DialogResult.OK)
+            if (MessageBox.Show($"确定要删除文明名【{fileName}】? 当前操作不可逆~", "",
+                                                           MessageBoxButtons.OKCancel) == DialogResult.OK)
             {
-                using (var client = new HttpClient())
+
+                ResultMessage resultMessage = await _fls.DeleteFile(fileName);
+                if (resultMessage.Status)
                 {
-                    var url = ConfigurationManager.AppSettings["DelInfo"].ToString();
-                    url += $"?fileName={fileName}";
-                    var response = client.DeleteAsync(url).Result;
-                    string json = await response.Content.ReadAsStringAsync();
-                    ResultMessage resultMessage = JsonConvert.DeserializeObject<ResultMessage>(json);
-                    if (resultMessage.Status)
-                    {
-                        WriteMsg($"【{fileName}】删除成功~");
-                    }
-                    else
-                    {
-                        WriteMsg($"【{fileName}】删除失败:{resultMessage.Message}~");
-                    }
+                    WriteMsg($"【{fileName}】删除成功~");
+                }
+                else
+                {
+                    WriteMsg($"【{fileName}】删除失败:{resultMessage.Message}~");
                 }
 
                 btnRefresh_ClickAsync(sender, e);
---
+++ WinformUDload/Services/FileLoadServices.cs
@@ -0,0 +1,150 @@
+﻿using Masuit.Tools.Database;
+using Masuit.Tools.Files;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinformUDload.Services
+{
+    public class FileLoadServices
+    {
+        public async Task<ResultMessage> DeleteFile(string fileName)
+        {
+            using (var client = new HttpClient())
+            {
+                var url = ConfigurationManager.AppSettings["DelInfo"].ToString();
+                url += $"?fileName={fileName}";
+                var response = client.DeleteAsync(url).Result;
+                string json = await response.Content.ReadAsStringAsync();
+                ResultMessage resultMessage = JsonConvert.DeserializeObject<ResultMessage>(json);
+                return resultMessage;
+            }
+        }
+
+        /// <summary>
+        /// 获取可以更新文件信息
+        /// </summary>
+        /// <returns></returns>
+        public async Task<List<FileInformation>> GetNeedDownFiles()
+        {
+            List<FileDownloadDto> list = new List<FileDownloadDto>();
+            string location = Assembly.GetEntryAssembly().Location;
+            List<FileInformation> fileInformations = await GetWebFiles();
+            string[] localPathArray = (from a in Directory.GetFiles(Path.GetDirectoryName(location))
+                                       join b in fileInformations on Path.GetFileName(a) equals b.FileName
+                                       select a).ToArray();
+
+            for (int i = 0; i < localPathArray.Length; i++)
+            {
+                string itemPath = localPathArray[i];
+                using (var fs = new FileStream(itemPath, FileMode.Open))
+                {
+                    list.Add(new FileDownloadDto
+                    {
+                        FileName = Path.GetFileName(itemPath),
+                        FileMd5 = fs.GetFileMD5()
+                    });
+                }
+            }
+
+            var needDownInfos = (from a in fileInformations
+                                 join b in list on a.FileName equals b.FileName
+                                 where a.FileName == b.FileName && a.FileMd5 != b.FileMd5
+                                 select a).
+                                 Concat(
+                                 from a in fileInformations
+                                 where !(from b in list select b).Any(b => a.FileName == b.FileName)
+                                 select a).ToList();
+            return needDownInfos;
+        }
+
+
+        /// <summary>
+        /// 下载文件
+        /// </summary>
+        /// <param name="fileName">文件名</param>
+        /// <returns></returns>
+        public async Task<ResultMessage> DownFile(string fileName)
+        {
+            ResultMessage resultMessage = null;
+            try
+            {
+                using (var client = new HttpClient())
+                {
+                    var url = ConfigurationManager.AppSettings["DownFile"].ToString();
+                    url += "?fileName=" + fileName;
+                    var response = await client.GetAsync(url);
+                    if (response.IsSuccessStatusCode)
+                    {
+                        using (var stream = await response.Content.ReadAsStreamAsync())
+                        {
+                            string winPath = Path.Combine(Path.GetDirectoryName(Assembly.GetEntryAssembly().Location), fileName);
+                            await stream.CopyToFileAsync(winPath);
+                            resultMessage = new ResultMessage { Status = true, Message = "成功" };
+                        }
+                    }
+                    else
+                    {
+                        resultMessage = new ResultMessage { Status = false, Message = response.ToString() };
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                resultMessage = new ResultMessage { Status = false, Message = ex.Message };
+            }
+
+            return resultMessage;
+        }
+
+        /// <summary>
+        ///   文件上传
+        /// </summary>
+        /// <param name="filePath">本地需要上传的文件路径</param>
+        /// <returns></returns>
+        public async Task<FileUpLoadDto> FileUpload(string filePath)
+        {
+            FileUpLoadDto fileUpLoadDto = null;
+            using (var httpClient = new HttpClient())
+            {
+                var fs = new FileStream(filePath, FileMode.Open, FileAccess.Read);
+                var fileName = Path.GetFileName(filePath);
+                var content = new MultipartFormDataContent();
+                //  Add 方法 第一个参数是content 
+                content.Add(new StreamContent(fs), "file", fileName);
+                var url = ConfigurationManager.AppSettings["Upload"].ToString();
+                var response = await httpClient.PostAsync(url, content);
+                var json = await response.Content.ReadAsStringAsync();
+                fileUpLoadDto = JsonConvert.DeserializeObject<FileUpLoadDto>(json);
+            }
+            return fileUpLoadDto;
+        }
+
+        public async Task<DataTable> GetWebFilesDataTable()
+        {
+            List<FileInformation> fileInformations = await GetWebFiles();
+            return fileInformations.ToDataTable("fileInformations");
+
+        }
+
+        public async Task<List<FileInformation>> GetWebFiles()
+        {
+            using (HttpClient httpClient = new HttpClient())
+            {
+                var url = ConfigurationManager.AppSettings["GetInfo"].ToString();
+                var response = await httpClient.GetAsync(url);
+                string json = await response.Content.ReadAsStringAsync();
+                var fileInformations = JsonConvert.DeserializeObject<List<FileInformation>>(json);
+                return fileInformations;
+            }
+        }
+    }
+}
